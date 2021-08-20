03a3b0bf1e8ef81b34a5fcc7b4027adf8fb85ace
Leon <leon.gugel@d-id.com>
2021-08-20 21:53:35 +0300

    eval mterics for ref and ae

--- training/phase_retrival_evaluator.py
+++ training/phase_retrival_evaluator.py
@@ -2,6 +2,7 @@ import numpy as np
 import fire
 import os
 from tqdm import tqdm
+from typing import List
 import pandas as pd
 from dataclasses import dataclass, field
 from skimage.metrics import structural_similarity as _ssim
@@ -52,6 +53,8 @@ class TrainerPhaseRetrievalEvaluator(BaseTrainerPhaseRetrieval):
         self._generator_model = PhaseRetrievalAeModel(config=self._config, s3=self._s3, log=self._log)
         self._generator_model.set_eval_mode()
         self._generator_model.set_device(self.device)
+
+        self._log.debug(f'loaded_sate from {model_path}')
         loaded_sate = self.load_state(model_path)
         self._generator_model.load_modules(loaded_sate, force=True)
 
@@ -73,22 +76,44 @@ class TrainerPhaseRetrievalEvaluator(BaseTrainerPhaseRetrieval):
             inv_norm = self.test_ds.get_inv_normalize_transform()
         elif type_ds == 'train':
             p_bar_data_loader = tqdm(self.train_paired_loader)
-            inv_norm = self.train_paired_loader.get_inv_normalize_transform()
+            inv_norm = self.train_ds.get_inv_normalize_transform()
         else:
             raise NameError(f'Non valid ds type: {type_ds}, must train/test')
 
-        evaluation_metrics = []
+        eval_metrics_recon_ref_net = []
+        eval_metrics_recon = []
+        eval_metrics_ae = []
         for batch_idx, data_batch in enumerate(p_bar_data_loader):
             data_batch = self.prepare_data_batch(data_batch)
             inferred_batch = self._generator_model.forward_magnitude_encoder(data_batch, eval_mode=False)
             gt_images = inv_norm(data_batch.image).detach().cpu().numpy()
-            predicted_images = inv_norm(inferred_batch.img_recon_ref).detach().cpu().numpy()
-            for gt_img, pred_img in zip(gt_images, predicted_images):
-                evaluation_metrics.append(CalculateMetrics.metrics(gt_img, pred_img))
+            recon_ref_images = inv_norm(inferred_batch.img_recon_ref).detach().cpu().numpy()
+            recon_images = inv_norm(inferred_batch.img_recon).detach().cpu().numpy()
+            ae_images = inv_norm(inferred_batch.decoded_img).detach().cpu().numpy()
+            for gt_img, recon_ref_img, recon_img, ae_img in zip(gt_images, recon_ref_images, recon_images, ae_images):
+                eval_metrics_recon_ref_net.append(CalculateMetrics.metrics(gt_img, recon_ref_img))
+                eval_metrics_recon.append(CalculateMetrics.metrics(gt_img, recon_img))
+                eval_metrics_ae.append(CalculateMetrics.metrics(gt_img, ae_img))
 
+        eval_recon_ref_net_df = self._get_eval_df(eval_metrics_recon_ref_net)
+        eval_recon_df = self._get_eval_df(eval_metrics_recon)
+        eval_ae_df = self._get_eval_df(eval_metrics_ae)
+        eval_df = pd.concat([eval_recon_ref_net_df, eval_recon_df, eval_ae_df],
+                            keys=['recon_ref_net', 'recon', 'ae'])
+
+        self._log.debug(f'\n{eval_df}')
+
+        save_out_url_csv = os.path.join(save_out_url, f'eval-metrics-{type_ds}.csv')
+        self._s3.save_object(save_out_url_csv, saver=lambda save_path: eval_df.to_csv(save_path))
+        self.eval_dbg_batch(save_out_url)
+        return save_out_url_csv
+
+    @staticmethod
+    def _get_eval_df(evaluation_metrics: List[EvaluationMetrics]) -> pd.DataFrame:
         evaluation_metrics = EvaluationMetrics.merge(evaluation_metrics)
         keys = evaluation_metrics.get_keys()
-        eval_df = pd.DataFrame(index=keys, columns=['mean', 'std', 'min', 'max'])
+        eval_df = pd.DataFrame(index=keys,
+                               columns=['mean', 'std', 'min', 'max'])
         mean_metrics = evaluation_metrics.mean()
         std_metrics = evaluation_metrics.std()
         min_metrics = evaluation_metrics.min()
@@ -98,17 +123,11 @@ class TrainerPhaseRetrievalEvaluator(BaseTrainerPhaseRetrieval):
             eval_df.loc[key]['std'] = std_metrics.as_dict()[key]
             eval_df.loc[key]['min'] = min_metrics.as_dict()[key]
             eval_df.loc[key]['max'] = max_metrics.as_dict()[key]
-
-        self._log.debug(f'\n{eval_df}')
-
-        save_out_url_csv = os.path(save_out_url, f'eval-metrics-{type_ds}.csv')
-        self._s3.save_object(save_out_url_csv, saver=lambda save_path: eval_df.to_csv(save_path))
-        self.eval_dbg_batch(save_out_url)
-        return save_out_url_csv
+        return eval_df
 
     def eval_dbg_batch(self, save_url: str):
-        inferred_batch_tr = self._generator_model.forward_magnitude_encoder(self.data_tr_batch, eval_mode=False)
-        inferred_batch_ts = self._generator_model.forward_magnitude_encoder(self.data_ts_batch, eval_mode=False)
+        inferred_batch_tr = self._generator_model.forward_magnitude_encoder(self.data_tr_batch, eval_mode=True)
+        inferred_batch_ts = self._generator_model.forward_magnitude_encoder(self.data_ts_batch, eval_mode=True)
         self._save_dbg_img(self.data_tr_batch, inferred_batch_tr, save_url, 'tr')
         self._save_dbg_img(self.data_ts_batch, inferred_batch_ts, save_url, 'ts')
 
