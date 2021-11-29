10747115a65f4f43931b4f45fea73bae9ed58fcf
Leon <leon.gugel@d-id.com>
2021-11-29 21:30:56 +0200

    bug fixed

--- training/phase_retrieval_trainer.py
+++ training/phase_retrieval_trainer.py
@@ -695,8 +695,8 @@ class TrainerPhaseRetrievalAeFeatures(BaseTrainerPhaseRetrieval):
             enc_layers_features_ts = enc_layers_list(self.data_ts_batch.image, use_residual=True)[1:]
 
             dec_layers_list = self._generator_model.ae_net._decoder.get_layers()
-            dec_layers_features_tr = dec_layers_list(self.data_tr_batch.image, use_residual=True)[1:]
-            dec_layers_features_ts = dec_layers_list(self.data_ts_batch.image, use_residual=True)[1:]
+            dec_layers_features_tr = dec_layers_list(enc_layers_features_tr[-1], use_residual=True)[1:]
+            dec_layers_features_ts = dec_layers_list(enc_layers_features_ts[-1], use_residual=True)[1:]
 
         img_grid_tr = self._grid_images(self.data_tr_batch, recon_data_tr_batch)
         img_grid_ts = self._grid_images(self.data_ts_batch, recon_data_ts_batch)
